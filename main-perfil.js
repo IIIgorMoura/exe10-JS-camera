de733e36bccce3558628814b1b026a6f3c8b6ce2
IIIgorMoura <igor2007moura@gmail.com>
2023-11-27 11:36:11 -0300

    a

--- main-perfil.js
+++ main-perfil.js
@@ -147,3 +147,50 @@ function popupFoto() {
 function fecharPopupFoto() {
     popupImagem.style.display = "none";
 }
+
+// gerar foto
+var mediaStream;
+
+function abrirCamera() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(function (stream) {
+            mediaStream = stream;
+            const areaVideo = document.getElementById('camera');
+            areaVideo.srcObject = stream;
+        })
+        .catch(function (error) {
+            console.error('Erro ao acessar a câmera', error)
+        })
+};
+
+function tirarFoto() {
+    const areaVideo = document.getElementById('camera');
+    const canvas = document.createElement('canvas');
+
+    canvas.width = areaVideo.videoWidth;
+    canvas.height = areaVideo.videoHeight;
+
+    const context = canvas.getContext('2d');
+
+    context.drawImage(areaVideo, 0, 0, canvas.width, canvas.height)
+
+    const imageDataURL = canvas.toDataURL();
+    const fotoDiv = document.getElementById('foto');
+
+    fotoDiv.style.backgroundImage = `url(${imageDataURL})`;
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = imageDataURL;
+    downloadLink.download = 'foto.png';
+    downloadLink.textContent = 'Clique para baixar';
+    document.body.appendChild(downloadLink);
+}
+
+function fecharCamera() {
+    navigator.mediaDevices.getUserMedia({ video: false });
+
+    const areaVideo = document.getElementById('camera')
+
+    areaVideo.srcObject = null;
+    mediaStream = null;
+}
